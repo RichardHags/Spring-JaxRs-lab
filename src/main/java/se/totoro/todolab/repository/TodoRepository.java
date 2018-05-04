2563a70d7940321a4827bea24f85e3411e424480
Richard <richardhags@gmail.com>
2018-05-04 09:09:20 +0200

    sista fix

--- src/main/java/se/totoro/todolab/repository/TodoRepository.java
+++ src/main/java/se/totoro/todolab/repository/TodoRepository.java
@@ -2,16 +2,7 @@ package se.totoro.todolab.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import se.totoro.todolab.model.Todo;
-import se.totoro.todolab.model.User;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
-    List<Todo> getAllByUserId(Long id);
-
-    List<Todo> getAllByPriorityAndId(String priority, Long id);
-
-    Optional<User> getByUserId(Long id);
 }
